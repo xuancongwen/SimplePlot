2613bfd039a36009948197737aa24bb0e6845fd8
xuancong <xuancongwen@gmail.com>
2010-07-30 19:07:29 -0700

    Added a histogram view stub.

---
+++ Classes/SPHistogramView.h
@@ -0,0 +1,16 @@
+//
+//  SPHistogramView.h
+//  SimplePlot
+//
+//  Created by Sam Wen on 7/30/10.
+//  Copyright 2010 Square. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SPHistogramView : SPCartesianPlotView {
+
+}
+
+@end
---
+++ Classes/SPHistogramView.m
@@ -0,0 +1,26 @@
+//
+//  SPHistogramView.m
+//  SimplePlot
+//
+//  Created by Sam Wen on 7/30/10.
+//  Copyright 2010 Square. All rights reserved.
+//
+
+#import "SPHistogramView.h"
+
+
+@implementation SPHistogramView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
